1a44f1d753788c24308f18918a344f47db11c32d
Gareth <me@gareth.codes>
2015-06-28 08:48:03 -0700

    testling badge

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ https://ampersandjs.com/docs/#ampersand-model
 https://developers.google.com/identity/protocols/OAuth2UserAgent
 https://ampersandjs.com/learn/events
 https://github.com/Raynos/xhr
+
+[![browser support](https://ci.testling.com/gyaresu/ampersand-model-testing.png)
+](https://ci.testling.com/gyaresu/ampersand-model-testing)
--- test/models.js
+++ test/models.js
@@ -3,8 +3,15 @@ var test = require('tape')
 var Person = require('../amp-m')
 
 test('person', function (t) {
-  t.plan(1)
+  t.plan(3)
 
-  var n = new Person({name: 'Gareth', greetingType: 'long'})
+  var n = new Person({name: 'Gareth', greetingType: 'short'})
+
   t.equal(n.name, 'Gareth', 'Person.name === "Gareth"')
+
+  t.equal(n.greeting, 'hi', 'Short greeting')
+
+  n.greetingType = 'long'
+
+  t.equal(n.greeting, 'hi Gareth', 'Long greeting')
 })
